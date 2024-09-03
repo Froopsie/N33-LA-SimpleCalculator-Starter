eec192fb78a0067b2510984388d0031096f63838
Christopher Hernandez <christopher24hd@gmail.com>
2024-09-03 13:27:04 -0400

    Added Changes (Just a test)

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -8,6 +8,8 @@ namespace SimpleCalculator
         {
             try
             {
+                /// CHANGE 2 & 3 (Chris)          
+
                 // Class to convert user input
                 InputConverter inputConverter = new InputConverter();
 
@@ -20,7 +22,7 @@ namespace SimpleCalculator
 
                 double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
 
-                Console.WriteLine(result);
+                Console.WriteLine("The final answer is: " + result);
 
             } catch (Exception ex)
             {
