902f7ca1924dbc71179c6edc9948d8df71129c25
Froopsie <jeremy.n.roos@gmail.com>
2024-09-13 21:47:49 -0400

    Final push

--- SimpleCalculator/CalculatorEngine.cs
+++ SimpleCalculator/CalculatorEngine.cs
@@ -1,42 +1,53 @@
 ﻿using System;
-using System.Reflection;
 
 namespace SimpleCalculator
 {
     public class CalculatorEngine
     {
-        public double Calculate (string argOperation, double argFirstNumber, double argSecondNumber)
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
         {
             double result = 0;
-            
-            switch (argOperation){
-                case "add":
-                case "+":
-                    result = argFirstNumber + argSecondNumber;
-                    break;
+            bool flag = false;
+            while (!flag)
+            {
+                switch (argOperation)
+                {
+                    case "add":
+                    case "+":
+                        result = argFirstNumber + argSecondNumber;
+                        flag = true;
+                        break;
 
-                case "minus":
-                case "-":
-                    result = argFirstNumber - argSecondNumber;
-                    break;
+                    case "minus":
+                    case "-":
+                        result = argFirstNumber - argSecondNumber;
+                        flag = true;
+                        break;
 
-                case "multiply":
-                case "*":
-                    result = argFirstNumber * argSecondNumber;
-                    break;
+                    case "multiply":
+                    case "*":
+                        result = argFirstNumber * argSecondNumber;
+                        flag = true;
+                        break;
 
-                case "divide":
-                case "/":
-                    result = argFirstNumber / argSecondNumber;
-                    break;
+                    case "divide":
+                    case "/":
+                        result = argFirstNumber / argSecondNumber;
+                        flag = true;
+                        break;
 
-                case "modulus":
-                case "%":
-                    result = argFirstNumber % argSecondNumber;
-                    break;
+                    case "modulus":
+                    case "%":
+                        result = argFirstNumber % argSecondNumber;
+                        flag = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid input : (+, -, \\, *, % or add, minus, multiply, divide, modulus)");
+                        break;
+                }
+
+                argOperation = Console.ReadLine();
             }
-            
-
             return result;
         }
     }
